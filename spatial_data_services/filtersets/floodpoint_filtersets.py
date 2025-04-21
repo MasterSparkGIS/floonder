44dd4b7c7699404c2e0857b3721f9a7c5affc7eb
Rahman Hakim <rahmanhakim2435@gmail.com>
2025-04-21 19:56:40 +0700

    use iexact

--- spatial_data_services/filtersets/floodpoint_filtersets.py
+++ spatial_data_services/filtersets/floodpoint_filtersets.py
@@ -2,10 +2,10 @@ import django_filters
 
 
 class FloodPointFilterset(django_filters.FilterSet):
-    kelurahan = django_filters.CharFilter(field_name='namobj', lookup_expr='icontains')
-    kecamatan = django_filters.CharFilter(field_name='wadmkc', lookup_expr='icontains')
+    kelurahan = django_filters.CharFilter(field_name='namobj', lookup_expr='iexact')
+    kecamatan = django_filters.CharFilter(field_name='wadmkc', lookup_expr='iexact')
     kota = django_filters.CharFilter(field_name='wadmkk', lookup_expr='iexact')
     provinsi = django_filters.CharFilter(field_name='wadmpr', lookup_expr='icontains')
-    month = django_filters.NumberFilter(field_name='month', lookup_expr='exact')
-    year = django_filters.NumberFilter(field_name='year', lookup_expr='exact')
+    month = django_filters.NumberFilter(field_name='month', lookup_expr='iexact')
+    year = django_filters.NumberFilter(field_name='year', lookup_expr='iexact')
 
